be05ce114bac1f65ce5031140294819ae32a29e8
BoltX <151031855+Kingfinik98@users.noreply.github.com>
2026-02-05 23:55:52 +0700

    add unoficial Manager Support

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -31,6 +31,26 @@
 #define EXPECTED_HASH_RESUKISU                                                 \
     "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"
 
+// MamboSU/MamboSU
+#define EXPECTED_SIZE_MAMBO 0x384
+#define EXPECTED_SIZE_MAMBO                                                 \
+    "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"
+
+// KOWX712/KernelSU
+#define EXPECTED_SIZE_KOWX712 0x375
+#define EXPECTED_HASH_KOWX712                                                 \
+    "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"
+    
+// KernelSU-Next/KernelSU-Next
+#define EXPECTED_SIZE_NEXT 0x3e6
+#define EXPECTED_SIZE_NEXT                                                 \
+    "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"
+    
+// KernelSU-WILD/KernelSU-WILD
+#define EXPECTED_SIZE_WILD 0x381
+#define EXPECTED_HASH_WILD                                                     \
+    "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4"
+
 // Dynamic Sign
 #define EXPECTED_SIZE_OTHER 0x300
 #define EXPECTED_HASH_OTHER                                                    \
